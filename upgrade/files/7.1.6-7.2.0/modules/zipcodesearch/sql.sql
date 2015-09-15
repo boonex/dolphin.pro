89987cbdbc6a363ef834c8745778a7ac8cc9cf53
camperjz <camperjz@hotmail.com>
2015-09-15 13:54:55 +0200

    Update sql.sql

--- upgrade/files/7.1.6-7.2.0/modules/zipcodesearch/sql.sql
+++ upgrade/files/7.1.6-7.2.0/modules/zipcodesearch/sql.sql
@@ -1,4 +1,14 @@
 
+DELETE FROM `sys_options` WHERE `Name` = 'bx_zip_geonames_username';
+
+SET @iCategId = (SELECT `ID` FROM `sys_options_cats` WHERE `name` = 'ZIP Code Search' LIMIT 1);
+
+INSERT INTO `sys_options` (`Name`, `VALUE`, `kateg`, `desc`, `Type`, `check`, `err_text`, `order_in_kateg`, `AvailableValues`) VALUES
+('bx_zip_geonames_username', '', @iCategId, 'Geonames username', 'digit', '', '', '16', '');
+
+UPDATE `sys_options` SET `order_in_kateg` = '10' WHERE `Name` = 'bx_zip_permalinks';
+UPDATE `sys_options` SET `order_in_kateg` = '12' WHERE `Name` = 'bx_zip_enabled';
+UPDATE `sys_options` SET `order_in_kateg` = '14' WHERE `Name` = 'bx_zip_mode';
 
 -- update module version
 
