66a28e5e9c0fc0f16ccf47bdd9608a1debc924d5
camperjz <camperjz@hotmail.com>
2015-09-15 12:49:00 +0200

    Create lang_ru.php

---
+++ upgrade/files/7.1.6-7.2.0/modules/events/lang_ru.php
@@ -0,0 +1,13 @@
+<?php
+
+$aLangContent = array(
+
+  // ================ ADDED KEYS ==================
+
+  '_bx_events_privacy_view_forum' => 'Читать форум доступно',
+
+  // ================ CHANGED KEYS ================
+
+  '_bx_events_privacy_post_in_forum' => 'Писать в форуме доступно',
+  
+);
