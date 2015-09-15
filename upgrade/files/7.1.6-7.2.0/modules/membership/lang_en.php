99d15179a9d9b188764e55e2669a277f678ad94d
camperjz <camperjz@hotmail.com>
2015-09-15 13:24:05 +0200

    Create lang_en.php

---
+++ upgrade/files/7.1.6-7.2.0/modules/membership/lang_en.php
@@ -0,0 +1,19 @@
+<?php
+
+$aLangContent = array(
+
+  // ================ ADDED KEYS ==================
+
+  '_membership_bcaption_error' => 'Error',
+  '_membership_bcaption_purchase_level' => 'Join by Selecting Membership Level',
+  '_membership_bcaption_select_level' => 'Select Level',
+  '_membership_btn_pay' => 'Checkout',
+  '_membership_err_need_select_level' => 'You need to select a level.',
+  '_membership_err_need_select_provider' => 'You need to select a payment provider.',
+  '_membership_err_no_payment_options' => 'There are no payment options available to join the site.',
+  '_membership_pcaption_error' => 'Error',
+  '_membership_smenu_item_join' => 'Join',
+  '_membership_txt_add_to_cart' => 'Add to Cart',
+  '_membership_txt_select_payment' => 'Select payment method',
+
+);
