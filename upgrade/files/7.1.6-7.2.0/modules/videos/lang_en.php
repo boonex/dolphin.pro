d1588a5b919cb4caf475389c47e61491472c183d
camperjz <camperjz@hotmail.com>
2015-09-15 13:49:15 +0200

    Create lang_en.php

---
+++ upgrade/files/7.1.6-7.2.0/modules/videos/lang_en.php
@@ -0,0 +1,12 @@
+<?php
+
+$aLangContent = array(
+
+  // ================ DELETED KEYS ================
+
+  /*
+  '_bx_videos_size_error' => 'File size can\'t be more than {0}',
+  '_bx_videos_upl_file_err' => 'Video upload failed (Video error)',
+  */
+
+);
