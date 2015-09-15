cb15fb3c35453784880503d5ecb2481e73dbc818
camperjz <camperjz@hotmail.com>
2015-09-15 13:24:50 +0200

    Create lang_ru.php

---
+++ upgrade/files/7.1.6-7.2.0/modules/membership/lang_ru.php
@@ -0,0 +1,19 @@
+<?php
+
+$aLangContent = array(
+
+  // ================ ADDED KEYS ==================
+
+  '_membership_bcaption_error' => 'Ошибка',
+  '_membership_bcaption_purchase_level' => 'Выберите членство для регистрации',
+  '_membership_bcaption_select_level' => 'Членство',
+  '_membership_btn_pay' => 'Оплатить',
+  '_membership_err_need_select_level' => 'Вам необходимо выбрать уровень членства.',
+  '_membership_err_need_select_provider' => 'Вам необходимо выбрать способ оплаты.',
+  '_membership_err_no_payment_options' => 'На данный момент нет вариантов оплаты для регистрации на сайте.',
+  '_membership_pcaption_error' => 'Ошибка',
+  '_membership_smenu_item_join' => 'Регистрация',
+  '_membership_txt_add_to_cart' => 'В корзину',
+  '_membership_txt_select_payment' => 'Выберите способ оплаты',
+
+);
