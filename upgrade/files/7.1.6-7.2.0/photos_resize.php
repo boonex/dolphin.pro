95c65ca06acedb2a7453b0960b1913962913fe13
camperjz <camperjz@hotmail.com>
2015-09-17 07:18:54 +0200

    Update photos_resize.php

--- upgrade/files/7.1.6-7.2.0/photos_resize.php
+++ upgrade/files/7.1.6-7.2.0/photos_resize.php
@@ -22,7 +22,7 @@ function PageCompPageMainCode()
         '_t.jpg' => array('w' => 240, 'h' => 240, 'square' => true),
         '_t_2x.jpg' => array('w' => 480, 'h' => 480, 'square' => true, 'new_file' => true),
     );
-    
+
     $sNewFile = false;
     foreach ($aNewSizes as $sKey => $r) {
         if (isset($r['new_file'])) {
@@ -30,41 +30,48 @@ function PageCompPageMainCode()
             break;
         }
     }
-    
+
     $sPathPhotos = BX_DIRECTORY_PATH_MODULES . 'boonex/photos/data/files/';
-    
-    $aRow = $GLOBALS['MySQL']->getFirstRow("SELECT `ID`, `Ext` FROM `bx_photos_main` ORDER BY `ID` ASC");
-    $iCounter = 0;
-    while (!empty($aRow)) {
-        $sFileOrig = $sPathPhotos . $aRow['ID'] . '.' . $aRow['Ext'];
-        $sFileNew = $sNewFile ? $sPathPhotos . $aRow['ID'] . $sNewFile : false;
-        
-        if ((!$sFileNew || !file_exists($sFileNew)) && file_exists($sFileOrig)) { // file isn't already processed and original exists
-            // resize
-            foreach ($aNewSizes as $sKey => $r) {
-                $sFileDest = $sPathPhotos . $aRow['ID'] . $sKey;
-                imageResize($sFileOrig, $sFileDest, $r['w'], $r['h'], true, $r['square']);
-            }
-            ++$iCounter;
-        }
-        
-        if ($iCounter >= $iLimit)
-            break;
+
+    if ($GLOBALS['MySQL']->getOne("SELECT COUNT(*) FROM `sys_modules` WHERE `uri` IN('photos')")) {
+
+        $aRow = $GLOBALS['MySQL']->getFirstRow("SELECT `ID`, `Ext` FROM `bx_photos_main` ORDER BY `ID` ASC");
+        $iCounter = 0;
+        while (!empty($aRow)) {
+            $sFileOrig = $sPathPhotos . $aRow['ID'] . '.' . $aRow['Ext'];
+            $sFileNew = $sNewFile ? $sPathPhotos . $aRow['ID'] . $sNewFile : false;
             
-        $aRow = $GLOBALS['MySQL']->getNextRow();
-    }
-    
-    if (empty($aRow)) {
-        $s = "All photos has been resized to the new dimentions.";
+            if ((!$sFileNew || !file_exists($sFileNew)) && file_exists($sFileOrig)) { // file isn't already processed and original exists
+                // resize
+                foreach ($aNewSizes as $sKey => $r) {
+                    $sFileDest = $sPathPhotos . $aRow['ID'] . $sKey;
+                    imageResize($sFileOrig, $sFileDest, $r['w'], $r['h'], true, $r['square']);
+                }
+                ++$iCounter;
+            }
+
+            if ($iCounter >= $iLimit)
+                break;
+
+            $aRow = $GLOBALS['MySQL']->getNextRow();
+        }
+
+        if (empty($aRow)) {
+            $s = "All photos has been resized to the new dimentions.";
+        }
+        else {
+            $s = "Page is reloading to resize next bunch of images...
+            <script>
+                setTimeout(function () {
+                    document.location = '" . BX_DOL_URL_ROOT . "upgrade/files/7.1.6-7.2.0/photos_resize.php?_t=" . time() . "';
+                }, 1000);
+            </script>";
+        }
+
     }
     else {
-        $s = "Page is reloading to resize next bunch of images...
-        <script>
-            setTimeout(function () {
-                document.location = '" . BX_DOL_URL_ROOT . "upgrade/files/7.1.6-7.2.0/photos_resize.php?_t=" . time() . "';
-            }, 1000);
-        </script>";
+        $s = "Module 'Photos' isn't installed";
     }
-    
+
     return DesignBoxContent($GLOBALS['_page']['header'], $s, $GLOBALS['oTemplConfig'] -> PageCompThird_db_num);
 }
