721f28ec0a701bc1e650b480331ddc573beea74a
Anton Lesnikov <vlnotna@gmail.com>
2015-12-18 08:45:04 +0300

    Ticket #120

--- inc/classes/BxDolForm.php
+++ inc/classes/BxDolForm.php
@@ -502,7 +502,7 @@ class BxDolFormCheckerHelper
     }
     function checkEmail($s)
     {
-        return $this->checkPreg ($s, "/^[a-z0-9_\-]+(\.[_a-z0-9\-]+)*@([_a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel)$/");
+        return filter_var($s, FILTER_VALIDATE_EMAIL) !== false;
     }
     function checkCaptcha($s)
     {
