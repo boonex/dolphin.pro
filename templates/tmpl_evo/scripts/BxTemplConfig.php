ace427e96987f7fdb91a6549ef732af4ac9692b0
Alex Trofimov <alex.trofimov@boonex.com>
2016-01-19 07:49:10 +0600

    #235 Zodiac signs aren't working in EVO template

--- templates/tmpl_evo/scripts/BxTemplConfig.php
+++ templates/tmpl_evo/scripts/BxTemplConfig.php
@@ -13,6 +13,7 @@ require_once( BX_DIRECTORY_PATH_BASE . 'scripts/BxBaseConfig.php' );
 
 // path to the images used in the template
 $site['images']	= $site['url'] . "templates/tmpl_{$GLOBALS['tmpl']}/images/";
+$site['zodiac']	= $site['url'] . "templates/base/images/zodiac/";
 $site['icons']	= $site['images'] . "icons/";
 $site['css_dir']= "templates/tmpl_{$GLOBALS['tmpl']}/css/";
 
