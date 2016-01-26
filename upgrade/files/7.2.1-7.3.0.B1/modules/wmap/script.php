69f9a0920e66a514e6039e4a982a18bfc58e9d5f
Alex Trofimov <alex.trofimov@boonex.com>
2016-01-27 04:50:18 +0600

    Upgrade script from 7.2.1 to 7.3.0.B1

--- upgrade/files/7.2.1-7.3.0.B1/modules/wmap/script.php
+++ upgrade/files/7.2.1-7.3.0.B1/modules/wmap/script.php
@@ -1,6 +1,6 @@
 <?php
 
-    $aFields = $this->oDb->getFields('bx_wmap_parts`');
+    $aFields = $this->oDb->getFields('bx_wmap_parts');
 
     if (!in_array('join_field_longitude', $aFields['original']))
         $this->oDb->res("ALTER TABLE `bx_wmap_parts` ADD `join_field_longitude` varchar(64) NOT NULL AFTER `join_field_address`");
