84b80de86b1683dc605f5323a6837a24419a1e24
Alex Trofimov <alex.trofimov@boonex.com>
2016-02-29 09:00:43 +0600

    Upgrade script from 7.2.0.B2 to 7.3.0.RC

--- upgrade/files/7.3.0.B2-7.3.0.RC/modules/wall/lang_en.php
+++ upgrade/files/7.3.0.B2-7.3.0.RC/modules/wall/lang_en.php
@@ -4,7 +4,16 @@ $aLangContent = array(
 
   // ================ ADDED KEYS ==================
 
+  '_wall_hide_comments' => 'Hide comments',
+  '_wall_n_comments_long' => '<i>{0}</i> comments',
+  '_wall_n_reposts' => '<i>{0}</i>',
+  '_wall_n_reposts_long' => '<i>{0}</i> reposts',
+  '_wall_n_votes' => '<i>{0}</i>',
+  '_wall_n_votes_long' => '<i>{0}</i> likes',
+  '_wall_no_reposts' => '',
+  '_wall_no_reposts_long' => '<i>{0}</i> reposts',
   '_wall_select_file' => 'Select File',
+  '_wall_show_comments' => 'Show comments',
 
   // ================ CHANGED KEYS ==================
 
@@ -17,5 +26,14 @@ $aLangContent = array(
   '_wall_alert_action_delete' => '{0} (delete item)',
   '_wall_alert_action_edit' => '{0} (change item)',
   '_wall_alert_action_edit_status_message' => '{0} (change status message)',
+  '_wall_n_comments' => '<i>{0}</i>',
+  '_wall_post_delete' => 'Delete',
+
+  // ================ DELETED KEYS ==================
+
+/*
+  '_wall_hide_n_comments' => 'Hide {0} comments',
+  '_wall_show_n_comments' => 'Show {0} comments',
+*/
 
 );
