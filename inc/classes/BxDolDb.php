fe54ec31fa812bd0e6e9479a1b7f998c09608bcf
Prashank Abhishek <prashank@codesm.com>
2016-06-28 06:47:05 +0530

    use presistent connection

--- inc/classes/BxDolDb.php
+++ inc/classes/BxDolDb.php
@@ -106,7 +106,8 @@ class BxDolDb
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=""',
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false
+                PDO::ATTR_EMULATE_PREPARES   => false,
+                PDO::ATTR_PERSISTENT         => true
             ]
         );
     }
@@ -433,7 +434,7 @@ class BxDolDb
 
     /**
      * Retuns last insert id
-     * 
+     *
      * @return string
      */
     public function lastId()
