a93ec89380df8c9f1839351829c381d622f4e6f6
Anton Lesnikov <vlnotna@gmail.com>
2016-07-06 07:41:44 +0300

    Ticket #413

--- modules/boonex/events/classes/BxEventsModule.php
+++ modules/boonex/events/classes/BxEventsModule.php
@@ -916,10 +916,11 @@ class BxEventsModule extends BxDolTwigModule
         if (!$aEvent['EventMembershipFilter']) {
             return true;
         }
+
         require_once(BX_DIRECTORY_PATH_INC . 'membership_levels.inc.php');
         $aMemebrshipInfo = getMemberMembershipInfo($this->_iProfileId);
 
-        return $aEvent['EventMembershipFilter'] == $aMemebrshipInfo['ID'] && $aMemebrshipInfo['DateStarts'] < time() && $aMemebrshipInfo['DateExpires'] > time() ? true : false;
+        return $aEvent['EventMembershipFilter'] == $aMemebrshipInfo['ID'];
     }
 
     function isAllowedSendInvitation(&$aEvent)
