4c81bfbbf7b133cb10a0e933b01288be428a9ad6
Anton Lesnikov <vlnotna@gmail.com>
2016-07-06 10:57:02 +0300

    Ticket #403

--- modules/boonex/oauth2/install/installer.php
+++ modules/boonex/oauth2/install/installer.php
@@ -11,16 +11,43 @@ class BxOAuthInstaller extends BxDolInstaller
     function BxOAuthInstaller($aConfig)
     {
         parent::BxDolInstaller($aConfig);
+
+        $this->_aActions['check_requirements'] = array(
+			'title' => 'Check requirements:',
+		);
     }
 
-    function install($aParams)
-    {
-        $aResult = parent::install($aParams);
-        return $aResult;
-    }
+	function actionCheckRequirements()
+	{
+		$aPhpSettings = array (
+			'php module: pdo' => array('op' => 'module', 'val' => 'pdo'),
+            'php module: pdo_mysql' => array('op' => 'module', 'val' => 'pdo_mysql')
+		);
 
-    function uninstall($aParams)
-    {
-        return parent::uninstall($aParams);
-    }
+		bx_import('BxDolAdminTools');
+		$oAdmTools = new BxDolAdminTools();
+
+		$aResult = array();
+		foreach ($aPhpSettings as $sName => $aData) {
+            $aCheckResult = $oAdmTools->checkPhpSetting($sName, $aData);
+            if($aCheckResult['res'])
+            	continue;
+
+			$aResult[] = $aData['val'];
+		}
+
+		return empty($aResult) ? BX_DOL_INSTALLER_SUCCESS : array('code' => BX_DOL_INSTALLER_FAILED, 'data' => $aResult);
+	}
+
+	function actionCheckRequirementsFailed($mixedResult)
+	{
+		if(empty($mixedResult['data']))
+			return $this->actionOperationFailed($mixedResult);
+
+		$sResult = 'The following PHP modules are required:<br />';
+        foreach($mixedResult['data'] as $sModule)
+            $sResult .= '-- ' . $sModule . '<br />';
+
+        return $sResult;
+	}
 }
