5fe542a61e640d1ce4407e004e74d1fd86f9deb4
Alex Trofimov <alex.trofimov@boonex.com>
2016-07-20 09:45:57 +0600

    Upgrade script from 7.3.0 to 7.3.1

---
+++ upgrade/files/7.3.0-7.3.1/modules/custom_rss/sql.sql
@@ -0,0 +1,6 @@
+
+
+-- update module version
+
+UPDATE `sys_modules` SET `version` = '1.3.1' WHERE `uri` = 'custom_rss' AND `version` = '1.3.0';
+
---
+++ upgrade/files/7.3.0-7.3.1/sql.sql
@@ -0,0 +1,6 @@
+
+
+-- last step is to update current version
+
+UPDATE `sys_options` SET `VALUE` = '7.3.1' WHERE `Name` = 'sys_tmp_version';
+
