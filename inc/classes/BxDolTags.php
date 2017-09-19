583daaa10a2f8447faea8c55099fba495253a609
Anton Lesnikov <vlnotna@gmail.com>
2017-09-19 15:38:35 +0300

    Ticket #583

--- inc/classes/BxDolTags.php
+++ inc/classes/BxDolTags.php
@@ -23,6 +23,7 @@ class BxDolTags
     var $sCacheTable;
     var $sTagTable;
     var $aTagFields;
+    var $iTagLength;
 
     var $aTagObjects = array();
 
@@ -36,6 +37,7 @@ class BxDolTags
 
         $this->sCacheFile = 'sys_objects_tag';
         $this->sNonParseParams = 'tags_non_parsable';
+
         $this->sCacheTable = 'sys_objects_tag';
         $this->sTagTable = 'sys_tags';
         $this->aTagFields = array(
@@ -44,6 +46,8 @@ class BxDolTags
             'tag' => 'Tag',
             'date' => 'Date'
         );
+        $this->iTagLength = 32;
+
         $this->aObjFields = array(
             'id' => 'ID',
             'name' => 'ObjectName',
@@ -224,6 +228,8 @@ class BxDolTags
 
         foreach( $aTags as $sTag ) {
             $aTagsSet['tag'] = process_db_input(trim($sTag), BX_TAGS_STRIP, BX_SLASHES_NO_ACTION);
+            if(get_mb_len($aTagsSet['tag']) > $this->iTagLength)
+                $aTagsSet['tag'] = get_mb_substr($aTagsSet['tag'], 0, $this->iTagLength);
 
             $sQuery = "SELECT COUNT(*) FROM `sys_tags`
                 WHERE `ObjID` = '{$aTagsSet['id']}' AND `Type` = '{$aTagsSet['type']}' AND `Tag` = '{$aTagsSet['tag']}'";
