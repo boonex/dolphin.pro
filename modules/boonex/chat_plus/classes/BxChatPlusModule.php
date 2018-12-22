76ca4e523e78a8a2eeb98de8c7604fc46981d9aa
Alex Trofimov <alex.trofimov@boonex.com>
2018-12-22 21:39:33 +1100

    #640 Chat Plus: new livechat integration code

--- modules/boonex/chat_plus/classes/BxChatPlusModule.php
+++ modules/boonex/chat_plus/classes/BxChatPlusModule.php
@@ -81,16 +81,16 @@ class BxChatPlusModule extends BxDolConnectModule
 
         return <<<EOS
 
-<!-- Start of Helpdesk Livechat Script -->
+<!-- Start of Rocket.Chat Livechat Script -->
 <script type="text/javascript">
 (function(w, d, s, u) {
 	w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
 	var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
-	j.async = true; j.src = '{$sChatUrl}/packages/rocketchat_livechat/assets/rocket-livechat.js';
+	j.async = true; j.src = '{$sChatUrl}/packages/rocketchat_livechat/assets/rocketchat-livechat.min.js?_=201702160944';
 	h.parentNode.insertBefore(j, h);
 })(window, document, 'script', '{$sChatUrl}/livechat');
 </script>
-<!-- End of Helpdesk Livechat Script -->
+<!-- End of Rocket.Chat Livechat Script -->
 
 EOS;
 
